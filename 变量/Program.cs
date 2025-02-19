2332ae4a0ebfa6090cb0cda8ccdfd8927ffe21e0
Wu-Ke-CUC <wuke0704@outlook.com>
2025-02-19 21:28:33 +0800

    添加项目文件。

---
+++ 变量/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+//namespace Variables
+//{
+//    class Program1
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = 100;
+//            int m = 200;
+//            int sum = n + m;
+//            //m += 100;
+//            string name= "result is:";
+//            Console.WriteLine(name);
+//            Console.WriteLine(sum);
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+//namespace Variables
+//{
+//    class Program2
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Plase rnter your first number:");
+//            string str = Console.ReadLine();
+//            //强制类型转化
+//            //int result = int.Parse(str) + 20;
+//            string result = str + 20.ToString();
+//            Console.WriteLine("The result is:");
+//            Console.WriteLine(result);
+//
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+namespace Variables
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Plase rnter your name:");
+            string name = Console.ReadLine();
+
+            Console.Write("Plase rnter your age:");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + " and your age is " + age + ".");
+            Console.ReadKey();
+        }
+    }
+}
