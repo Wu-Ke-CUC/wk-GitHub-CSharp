b5c105dbf4f0f99cae3f443965bf8edd9ba77705
Wu-Ke-CUC <wuke0704@outlook.com>
2025-09-23 20:40:58 +0800

    Add files via upload

---
+++ 数组/数组/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 数组
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string>names=new List<string>();
+            //string[] names = new string[3];
+            names.Add("zhangsan");
+            names.Add("lisi");
+            names.Add("wangwu");
+            int n = 0;
+            for(int i=0;i<names.Count;i++)
+            {
+                if (names[i]=="zhangsan")
+                {
+                   n++;
+                }
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
