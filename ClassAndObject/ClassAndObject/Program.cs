2fbae6403a16d6a5a834640f0192e0e178033cb0
Wu-Ke-CUC <wuke0704@outlook.com>
2025-11-21 21:58:38 +0800

    25/11/21

--- ClassAndObject/ClassAndObject/Program.cs
+++ ClassAndObject/ClassAndObject/Program.cs
@@ -50,6 +50,8 @@ namespace ClassAndObject
             }
         }
 
+        //自动属性
+        public int HP{ get;set; }
         public Student(string name, int age, string id, StudentLevel studentLevel)
         {
             this.Name = name;
@@ -89,57 +91,124 @@ namespace ClassAndObject
     }
     #endregion
     #region Player
-    ///// <summary>
-    ///// Player类
-    ///// </summary>
-    //enum Equipment
-    //{
-    //    Dagger=10,
-    //    Rifle20,
-    //    MachineGun=30,
-    //    SniperRifle=100,
-    //}
-    //class Player
-    //{
-    //    public string name;
-    //    public string sex;
-    //    public int hp = 100;
-    //    public Equipment equipment;
+    /// <summary>
+    /// Player类
+    /// </summary>
+    enum Equipment
+    {
+        Dagger = 10,
+        Rifle20,
+        MachineGun = 30,
+        SniperRifle = 100,
+    }
+    class Player
+    {
+        public string name;
+        public string sex;
+        public int hp = 100;
+        public Equipment equipment;
 
-    //    public void Introduce()
-    //    {
-    //        Console.WriteLine($"Your name is {name},your sex is {sex},your HP is {hp},and you equiped {equipment}.");
-    //    }
-    //    public void ChangeEquipment(Equipment equipment)
-    //    {
-    //        this.equipment = equipment;
-    //    }
-    //    public void Struck(int damage)
-    //    {
-    //        hp -= damage;
-    //        if(hp<=0)
-    //        {
-    //            Console.WriteLine("You  have dead");
-    //        }
-    //    }
-    //    public void Attack(Player other)
-    //    {
-    //        other.hp -= (int)equipment;
-    //    }
-    //}
+        public void Introduce()
+        {
+            Console.WriteLine($"Your name is {name},your sex is {sex},your HP is {hp},and you equiped {equipment}.");
+        }
+        public void ChangeEquipment(Equipment equipment)
+        {
+            this.equipment = equipment;
+        }
+        public void Struck(int damage)
+        {
+            hp -= damage;
+            if (hp <= 0)
+            {
+                Console.WriteLine("You  have dead");
+            }
+        }
+        public void Attack(Player other)
+        {
+            other.hp -= (int)equipment;
+        }
+    }
+    #endregion
+    #region UltramanAndMonster
+    class Ultraman
+    {
+        public string name;
+        private int attack;
+        public int Attack
+        {
+            get => attack;
+            set
+            {
+                if(value>0&&value<=100)
+                {
+                    attack = value;
+                }
+            }
+        }
+        public Ultraman(string name,int attack)
+        {
+            this.name = name;
+            Attack = attack;
+        }
+        public void AttackMonster(Monster monster)
+        {
+            monster.Hp -= Attack;
+        }
+    }
+    class Monster
+    {
+        public string name;
+        private int hp;
+        public int Hp
+        {
+            get => hp;
+            set
+            {
+                hp = value;
+                if (hp<0)
+                {
+                    Dead();
+                    hp = 0;
+                }
+            }
+        }
+        public Monster(string name,int hp)
+        {
+            this.name = name;
+            Hp = hp;
+        }
+        private void Dead()
+        {
+            Console.WriteLine($"{name} has dead");
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{name} remains {hp}HP");
+        }
+    }
     #endregion
     class Program
     {
         static void Main(string[] args)
         {
             #region Student
-            Student jay = new Student("Jay", 18, "2025", StudentLevel.top);//实例化//引用类型
-            jay.Study();
-            jay.Exam();
-            jay.IntroduceMyself();
+            //Student jay = new Student("Jay", 18, "2025", StudentLevel.top);//实例化//引用类型
+            //jay.Study();
+            //jay.Exam();
+            //jay.IntroduceMyself();
             #endregion
             #region Player
 
+            #endregion
+            #region UltramanAndMonster
+            //Ultraman ultraman = new Ultraman("迪迦", 30);
+            //Monster monster = new Monster("Gesla", 100);
+            //ultraman.AttackMonster(monster);
+            //monster.ShowInfo();
+            //ultraman.Attack = 100;
+            //ultraman.AttackMonster(monster);
+            //Console.WriteLine(monster.Hp);
             #endregion
         }
     }
