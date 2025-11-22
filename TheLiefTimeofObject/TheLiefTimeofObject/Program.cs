0e484d64ba4ffc543d94c83b8933a82e5858093d
WuKe <wuke0704@outlook.com>
2025-11-22 14:47:22 +0800

    Add files via upload

---
+++ TheLiefTimeofObject/TheLiefTimeofObject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLiefTimeofObject
+{
+    class Person
+    {
+        public string name;
+        public Person() { }
+        public Person(string name) { this.name = name; }
+        /// <summary>
+        /// 析构函数
+        /// </summary>
+        ~Person() { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
