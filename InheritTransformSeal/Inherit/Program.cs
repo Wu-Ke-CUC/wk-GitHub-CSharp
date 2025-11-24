96b72d042ca4983c0291530ec15d7c6657786139
Wu-Ke-CUC <wuke0704@outlook.com>
2025-11-25 00:12:05 +0800

    Inherit

--- InheritTransformSeal/Inherit/Program.cs
+++ InheritTransformSeal/Inherit/Program.cs
@@ -8,8 +8,51 @@ namespace Inherit
 {
     internal class Program
     {
+        class Person
+        {
+            public string name;
+            public int age;
+            public string sex;
+            public Person(string name,int age,string sex)
+            {
+                this.name = name;
+                this.age = age;
+                this.sex = sex;
+            }
+            public void Greet()
+            {
+                Console.WriteLine("Hello,my name is {0}.and I'm {1} years old.", name, age);
+            }
+        }
+        class Student : Person
+        {
+            public Student(string name, int age, string sex): base(name,age,sex)
+            {
+                this.name = name;
+                this.age = age;
+                this.sex = sex;
+            }
+            public void StudentGreet()
+            {
+                Console.WriteLine($"My name is {name},and I'm {age} years old,I like playing basketball.");
+            }
+        }
+        class Programmer : Person
+        {
+            public Programmer(string name, int age, string sex) : base(name, age, sex)
+            {
+                this.name = name;
+                this.age = age;
+                this.sex = sex;
+            }
+            public void ProgrammerGreet()
+            {
+                Console.WriteLine($"My name is {name},and I'm {age} years old,I like watching moives.");
+            }
+        }
         static void Main(string[] args)
         {
+            Student s = new Student("zhangsan",18,"male");
         }
     }
 }
