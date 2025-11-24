9cb80f9497e35f568363073fb22d07dca2b2c6bd
Wu-Ke-CUC <wuke0704@outlook.com>
2025-11-24 18:31:34 +0800

    拓展方法

---
+++ StaticVariableAndMethod/ExpendMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpendMethod
+{
+    static class Tools
+    {
+        /// <summary>
+        /// string首字母大写
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string FirstUpper(this string str)
+        {
+            char[] strs = str.ToCharArray();
+            strs[0] = str.ToUpper().ToCharArray()[0];
+            return new string(strs);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "student";
+            Console.WriteLine(str.FirstUpper());
+            str.ToUpper();
+        }
+    }
+}
