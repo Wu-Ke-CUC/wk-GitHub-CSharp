7ce7f978b28c5f589e72387cebfbe8a626da66c4
Wu-Ke-CUC <wuke0704@outlook.com>
2025-11-25 12:03:29 +0800

    Inherit

--- InheritTransformSeal/Inherit/Program.cs
+++ InheritTransformSeal/Inherit/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using InternalText;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +15,20 @@ namespace Inherit
             public string name;
             public int age;
             public string sex;
+            public float height;
+            //private只能在本类中使用
+            //protected只能在本类和子类中使用，外部无法访问
+            //internal只有当前程序集能使用
             public Person(string name,int age,string sex)
             {
                 this.name = name;
                 this.age = age;
                 this.sex = sex;
+            }
+            public Person(string name,int age,string sex,float height):this(name,age,sex)
+            {
+                //节省了重复代码
+                this.height = height;
             }
             public void Greet()
             {
@@ -26,15 +37,26 @@ namespace Inherit
         }
         class Student : Person
         {
-            public Student(string name, int age, string sex): base(name,age,sex)
+            //方法和变量与父类重名使，使用new关键字进行覆盖
+            //new public int age;
+            public string major;
+            public Student(string name, int age, string sex) : base(name, age, sex)
             {
                 this.name = name;
                 this.age = age;
                 this.sex = sex;
             }
-            public void StudentGreet()
+            public Student(string name,int age,string sex,string major):this(name,age,sex)
             {
-                Console.WriteLine($"My name is {name},and I'm {age} years old,I like playing basketball.");
+                this.major = major;
+            }
+            new public void Greet()
+            {
+                Console.WriteLine("Hello,my name is {0}.I'm {1} years old,and my major is {2}", name, age,major);
+            }
+            public void Hobby()
+            {
+                Console.WriteLine($"My name is {name},I like playing basketball.");
             }
         }
         class Programmer : Person
@@ -45,14 +67,32 @@ namespace Inherit
                 this.age = age;
                 this.sex = sex;
             }
-            public void ProgrammerGreet()
+            public void Hobby()
             {
-                Console.WriteLine($"My name is {name},and I'm {age} years old,I like watching moives.");
+                Console.WriteLine($"My name is {name},I like watching moives.");
             }
         }
         static void Main(string[] args)
         {
-            Student s = new Student("zhangsan",18,"male");
+            //Student s = new Student("zhangsan", 18, "male");
+            //Programmer p = new Programmer("wangwu", 20, "male");
+            //s.Greet();
+            //p.Greet();
+            //s.Hobby();
+            //p.Hobby();
+            Text1.TextWrite();
+            //Text2
+            //Console.WriteLine(Text.num);
+            //#region 代码块
+            //{
+            //    int a = 10;
+            //    Console.WriteLine(a);
+            //}
+            //Console.WriteLine(a);
+            //增加运行效率
+            //#endregion
+            Student s = new Student("hhl", 20, "male", "Unity");
+            s.Greet();
         }
     }
 }
---
+++ InheritTransformSeal/InternalText/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalText
+{
+    public static class Text1
+    {
+        internal static int num = 5;
+        public static void TextWrite()
+        {
+            Console.WriteLine($"你调用了TextWrite方法,num为{num}。");
+        }
+    }
+    internal static class Text2
+    {
+        internal static int num = 5;
+        public static void TextWrite()
+        {
+            Console.WriteLine($"你调用了TextWrite方法,num为{num}。");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
