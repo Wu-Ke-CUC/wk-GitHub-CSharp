a0b538dbef4ebb3ea77fae7b00000c6350db283e
Wu-Ke-CUC <wuke0704@outlook.com>
2025-11-29 23:12:19 +0800

    IndexerThread

---
+++ InterfaceIndexThread/Indexer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Person
+    {
+        public string name;
+        public string sex;
+        public int age;
+        public Person(string name, string sex, int age)
+        {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
+        //索引器
+        public string this[int x]
+        {
+            get
+            {
+                switch (x)
+                {
+                    case 0:return name;
+                    case 1:return sex;
+                    case 2: return age.ToString();
+                    default: return "数据异常";
+                }
+            }
+            set
+            {
+                switch (x)
+                {
+                    case 0: name = value;break;
+                    case 1: sex= value;break;
+                    case 2: age = int.Parse(value);break;
+                    default:break;
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("zhangsan", "male", 18);
+            Console.WriteLine(person[0]);
+            person[2] = "20";
+            Console.WriteLine(person.age);
+        }
+    }
+}
---
+++ InterfaceIndexThread/Threading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Threading
+{
+    internal class Program
+    {
+        static void TextThread()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("TextThread");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(TextThread);
+            thread.Start();
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                if (input == 't')
+                {
+                    thread.Abort();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(input);
+                }
+            }
+        }
+    }
+}
