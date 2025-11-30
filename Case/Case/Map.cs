4bc6cb9dba9d089f0fe8b4bfbbda32ce93f0842d
Wu-Ke-CUC <wuke0704@outlook.com>
2025-11-30 21:12:40 +0800

    MapandGameObject

---
+++ Case/Case/Map.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Map
+    {
+        string icon;
+        Vector2 size;
+        Vector2 startPos;
+        ConsoleColor color;
+        List<GameObject> gameObjects = new List<GameObject>();
+        public static Map instance;
+        public static Map Instance
+        {
+            get
+            {
+                return instance;
+            }
+            set
+            {
+                if(instance==null)
+                {
+                    instance = value;
+                }
+            }
+        }
+        public Map(string icon,Vector2 size,Vector2 startPos,ConsoleColor color)
+        {
+            this.icon = icon;
+            this.size = size;
+            this.startPos = startPos;
+            this.color = color;
+        }
+        public Map(string icon,int sizeX,int sizeY,int startPosX,int startPosY,ConsoleColor color):
+            this(icon,new Vector2(sizeX,sizeY),new Vector2(startPosX, startPosY), color) { }
+        public void GreateWallDate()
+        {
+            Console.ForegroundColor = color;
+            for (int i = 0; i < size.y; i++)
+            {
+                for (int j = 0; j < size.x; j++)
+                {
+                    if (i == 0||i==size.y-1) 
+                    {
+                        GameObject gameObject = new GameObject(new Vector2(startPos.x + j, startPos.y + i), icon, color);
+                        gameObjects.Add(gameObject);
+                        //gameObject.Draw();
+                        //Console.SetCursorPosition(startPos.x+j, startPos.y+i);
+                        //Console.Write(icon);
+                    }
+                    if(j==0||j==size.x-1)
+                    {
+                        GameObject gameObject = new GameObject(new Vector2(startPos.x + j, startPos.y + i), icon, color);
+                        gameObjects.Add(gameObject);
+                        //gameObject.Draw();
+                        //Console.SetCursorPosition(startPos.x + j, startPos.y + i);
+                        //Console.Write(icon);
+                    }
+                }
+            }
+        }
+        public void Draw()
+        {
+            foreach (var icon in gameObjects)
+            {
+                icon.Draw();
+            }
+        }
+    }
+}
--- Case/Case/Program.cs
+++ Case/Case/Program.cs
@@ -1,16 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SnakeGame
 {
+    struct Vector2
+    {
+        public int x, y;
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x + v2.x, v1.y + v2.y);
+        }
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.x - v2.x, v1.y - v2.y);
+        }
+        public static bool operator ==(Vector2 v1, Vector2 v2)
+        {
+            return ((v1.x == v2.x) && (v1.y == v2.y));
+        }
+        public static bool operator !=(Vector2 v1, Vector2 v2)
+        {
+            return !(v1 == v2);
+        }
+        public override string ToString()
+        {
+            return "x:" + x + " y:" + y;
+        }
+    }
+    class GameObject
+    {
+        public Vector2 pos;
+        public string icon;
+        public ConsoleColor color;
+        public GameObject(Vector2 pos, string icon, ConsoleColor color)
+        {
+            this.pos = pos;
+            this.icon = icon;
+            this.color = color;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(pos.x, pos.y);
+            Console.ForegroundColor = color;
+            Console.Write(icon);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void Remove()
+        {
+            Console.SetCursorPosition(pos.x, pos.y);
+            Console.Write(" ");
+        }
+        public void Move(Vector2 dir)
+        {
+            pos += dir;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            //Vector2 v1 = new Vector2(1, 2);
+            //Vector2 v2 = new Vector2(3, 4);
+            //Console.WriteLine(v1 + v2);
+            //GameObject g = new GameObject(new Vector2(3, 3), "*", ConsoleColor.Red);
+            //g.Draw();
+            //Console.ReadLine();
+            //g.Move(new Vector2(1, 1));
+            //g.Remove();
+            //g.Draw();
+            //Map map = new Map("*", new Vector2(10, 5), new Vector2(1, 1), ConsoleColor.Cyan);
+            //map.GreateWallDate();
+            Map.Instance= new Map("O", new Vector2(20, 20), new Vector2(5, 2), ConsoleColor.Cyan);
+            Map.Instance.GreateWallDate();
+            Map.Instance.Draw();
         }
     }
 }
