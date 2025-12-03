97082cc5100876fee4e96ac549f69ce3d2605bb5
Wu-Ke-CUC <wuke0704@outlook.com>
2025-12-03 23:48:58 +0800

    集合

--- GenericCollection/ArrayList/Program.cs
+++ GenericCollection/ArrayList/Program.cs
@@ -1,15 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArrayList
+namespace ArrayListUsing
 {
+    class Person
+    {
+        public string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            ArrayList arrayList = new ArrayList();
+            int[] ints = new int[4];
+            Person person = new Person("zhangsan");
+            arrayList.Add(1);
+            arrayList.Add(1.5f);
+            arrayList.Add(true);
+            arrayList.Add(ints);
+            Console.WriteLine(arrayList.Count);
+            Console.WriteLine(arrayList.Capacity);
+            arrayList.Add(person);
+            Console.WriteLine(arrayList.Count);
+            Console.WriteLine(arrayList.Capacity);
+            arrayList.Remove(ints);
+            arrayList.RemoveAt(0);
+            Console.WriteLine(arrayList[0]);
+            arrayList.Insert(1, "abc");
+            Console.WriteLine(arrayList[1]);
+            Console.WriteLine(arrayList.Count);
+            Console.WriteLine(arrayList.Capacity);
+            arrayList.Clear();
+            arrayList.Add(5);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Sort();//快排
+            foreach (var item in arrayList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            arrayList.Reverse();
+            foreach (var item in arrayList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(arrayList.Contains(3));
         }
     }
 }
---
+++ GenericCollection/HashTableUsing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableUsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(1, "1");
+            hashtable.Add("2", 2);
+            hashtable.Add("key", "value");
+            hashtable.Add("first", "HuNan");
+            Console.WriteLine(hashtable.ContainsKey(1));
+            Console.WriteLine(hashtable.ContainsValue("key"));
+            hashtable.Remove("2");
+            Console.WriteLine(hashtable.ContainsValue(2));
+            Console.WriteLine(hashtable["key"]);
+            foreach (var item in hashtable.Keys)
+            {
+                Console.Write(hashtable[item]+" ");
+            }
+            Console.WriteLine();
+            foreach (var item in hashtable.Values)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
