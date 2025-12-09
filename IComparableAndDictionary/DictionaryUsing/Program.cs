a324002b49ba75f778557c7576164eaebce24b19
Wu-Ke-CUC <wuke0704@outlook.com>
2025-12-09 15:15:07 +0800

    IComparable接口与Dictionary的使用

---
+++ IComparableAndDictionary/DictionaryUsing/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryUsing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students1 = new Dictionary<int, string>();
+            students1.Add(10001, "zhangsan");
+            students1.Add(10002, "lisi");
+            students1.Add(10003, "wangwu");
+            if (students1.ContainsKey(10001))
+            {
+                Console.WriteLine(students1[10001]);
+            }
+            foreach (var item in students1.Values)
+            {
+                Console.WriteLine(item);
+            }
+            string name;
+            if (students1.TryGetValue(10001, out name))
+            {
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
+            //<id,<name,score>> //将键值对数据类型作为值
+            Dictionary<int, KeyValuePair<string, int>> students2 = new Dictionary<int, KeyValuePair<string, int>>();
+            KeyValuePair<string, int> s1 = new KeyValuePair<string, int>("zhangsan", 100);
+            KeyValuePair<string, int> s2 = new KeyValuePair<string, int>("lisi", 90);
+            KeyValuePair<string, int> s3 = new KeyValuePair<string, int>("wangwu", 95);
+            students2.Add(10001,s1);
+            students2.Add(10002,s2);
+            students2.Add(10003,s3);
+            foreach (var item in students2.Keys)
+            {
+                Console.WriteLine($"id:{item} name:{students2[item].Key} score:{students2[item].Value}");
+            }
+        }
+    }
+}
--- IComparableAndDictionary/IComparableUsing/Program.cs
+++ IComparableAndDictionary/IComparableUsing/Program.cs
@@ -6,10 +6,82 @@ using System.Threading.Tasks;
 
 namespace IComparableUsing
 {
+    enum CompareType
+    {
+        Hp,Atk,Def
+    }
+
+    class Hero:IComparable<Hero>
+    {
+        public string name;
+        public int hp;
+        public int atk;
+        public int def;
+        public Hero(string name, int hp, int atk, int def)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.atk = atk;
+            this.def = def;
+        }
+        private static CompareType type = CompareType.Hp;
+        public static CompareType Type
+        {
+            set
+            {
+                type = value;
+            }
+        }
+        public int CompareTo(Hero other)
+        {
+            switch (type)
+            {
+                case CompareType.Hp:
+                    return hp - other.hp;
+                case CompareType.Atk:
+                    return atk - other.atk;
+                case CompareType.Def:
+                    return def - other.def;
+                default:
+                    return 0;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name:{name} HP:{hp} Atk:{atk} Def:{def}";
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Hero hero1 = new Hero("zhangsan",100,20,10);
+            Hero hero2 = new Hero("lisis",90,25,5);
+            Hero hero3 = new Hero("wangwu", 110, 15, 15);
+            List<Hero> heroes = new List<Hero>();
+            heroes.Add(hero1);
+            heroes.Add(hero2);
+            heroes.Add(hero3);
+            heroes.Sort();
+            foreach (var item in heroes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------");
+            Hero.Type = CompareType.Atk;
+            heroes.Sort();
+            foreach (var item in heroes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------");
+            Hero.Type = CompareType.Def;
+            heroes.Sort();
+            foreach (var item in heroes)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
